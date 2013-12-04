83551da7e85dff351cefc4844e6865c5e862f975
rlisam <rlisam@gmail.com>
2013-12-04 00:58:22 -0800

    Changed configuration in settings.py (untracked) and urls.py; replaced old files touched only by Cindy with new versions from repo; fixed links on menu

--- menu/urls.py
+++ menu/urls.py
@@ -2,5 +2,13 @@ from django.conf.urls import patterns, url
 from menu import views
 
 urlpatterns = patterns('',
-	url(r'main_menu/?$', views.main_menu, name='main_menu')
+	#url(r'calculate/?$', views.calculate, name='calculate'),
+	url(r'^accounts/login/$',  views.login, name='login'),
+	url(r'^accounts/auth_view/$', views.auth_view, name='auth_view'),
+	url(r'^accounts/logout/$', views.logout, name='logout'),
+    url(r'^accounts/loggedin/$', views.loggedin, name='loggedin'),
+    url(r'^accounts/invalid/$', views.invalid_login, name='invalid'),
+	url(r'^accounts/register/$', views.register, name='register'),
+	url(r'^accounts/register_success/$', views.register_success, name='register'),
+
 )
--- menu/views.py
+++ menu/views.py
@@ -1,9 +1,50 @@
-# Create your views here.
-from django.http import HttpResponse
-from django.shortcuts import render
+from menu.forms import User
 from django import forms
-from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, render_to_response
+from django.contrib import auth
+from django.core.context_processors import csrf
 
-def main_menu(request):
-    if request.method == 'GET':
-        return render( request, 'bootstrap_test.html')
+def auth_view(request):
+	username = request.POST.get('username', '')
+	password = request.POST.get('password', '')
+	user = auth.authenticate(username=username, password=password)
+	if user is not None and user.is_active:
+		auth.login(request, user)
+		return HttpResponseRedirect("/menu/accounts/loggedin/")
+	else:
+		return HttpResponseRedirect("/menu/accounts/invalid/")
+
+def invalid_login(request):
+	return render_to_response('invalid_login.html')
+
+def login(request):
+	c = {}
+	c.update(csrf(request))
+	return render_to_response('login.html',c)
+	
+def loggedin(request):
+	return render_to_response('loggedin.html', 
+		{'full_name': request.user.username})
+
+def logout(request):
+	auth.logout(request)
+	return HttpResponseRedirect("/menu/accounts/login/")
+
+def register(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/menu/accounts/register_success/')
+		else:
+			form = UserCreationForm()
+
+	args = {}
+	args.update(csrf(request))
+	args['form'] = UserCreationForm()
+	return render(request,'register.html', args)
+
+def register_success(request):
+	return render_to_response('register_success.html')
